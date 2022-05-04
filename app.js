08e65bcf516893d582a7af9a8583364613f46104
Jihong Min <jihong729@naver.com>
2022-05-04 23:43:08 +0900

    Finding(Reading) Documents

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  insertDocuments(db, function() {
-    client.close();
-  });
+  findDocuments(db, function() {
+      client.close();
+    });
 });
 
 
@@ -43,5 +43,17 @@ const insertDocuments = function(db, callback) {
     assert.equal(err, null);
     console.log("Inserted 3 documents into the collection");
     callback(result);
+  });
+}
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
   });
 }
