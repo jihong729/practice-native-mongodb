7088b503a42a423f69f1e593aec2cf1c95694d10
Jihong Min <jihong729@naver.com>
2022-05-06 18:35:35 +0900

    Connect the DB with Mongoose, delete fruitsDB, create studentsDB

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/studentsDB');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function() {
-      client.close();
-    });
+// Creating a new model
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number
 });
 
+const Student = mongoose.model('Student', studentSchema);
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([{
-    name: "Apple",
-    score: 8,
-    review: "Great fruit"
-  }, {
-    name: "Orange",
-    score: 6,
-    review: "Kinda sour"
-  }, {
-    name: "Banana",
-    score: 9,
-    review: "Great flavor!"
-  }], function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+// Creating a new document
+const student = new Student({ name: 'John', age: 35 });
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits)
-    callback(fruits);
-  });
-}
+// Saving the document
+student.save().then(() => console.log('Successfully saved'));
